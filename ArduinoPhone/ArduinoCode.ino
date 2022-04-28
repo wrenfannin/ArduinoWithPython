cee3cc0ea4f96c1155192482774b278eefb4491e
wrenfannin <64269332+wrenfannin@users.noreply.github.com>
2022-04-28 14:58:50 +0100

    Update ArduinoCode.ino

--- ArduinoPhone/ArduinoCode.ino
+++ ArduinoPhone/ArduinoCode.ino
@@ -37,12 +37,12 @@ void setup() {
   while (!Serial);
 
   Serial.begin(115200); // Sets the baud rate of serial monitor
-  Serial.println(F("FONA basic test")); // Prints message to show user tests are being run, can be removed
-  Serial.println(F("Initializing....(May take 3 seconds)"));
+  Serial.println(F("Testing Module")); // Prints message to show user tests are being run, can be removed
+  Serial.println(F("Initializing...."));
 
   fonaSerial->begin(4800);
   if (! fona.begin(*fonaSerial)) { // If there is no FONA (SIM) module wired to the arduino print this to the user
-    Serial.println(F("Couldn't find FONA"));
+    Serial.println(F("Couldn't find FONA module, check connections"));
     while (1);
   }
   type = fona.type(); // Holds the model of the module
@@ -64,13 +64,11 @@ void setup() {
 }
 
 void printMenu(void) {
-  Serial.println(F("-------------------------------------"));
-  Serial.println(F("[?] Print this menu"));
+  Serial.println(F("-------------------------------------")); // This menu is mostly for testing purposes but clearly shows how to access each feature
+  Serial.println(F("[?] Print this menu"));  // All of these options can be accessed via python with the Pyserial library
   Serial.println(F("[n] get Network status"));
   Serial.println(F("[v] set audio Volume"));
   Serial.println(F("[V] get Volume"));
-  Serial.println(F("[H] set Headphone audio"));
-  Serial.println(F("[e] set External audio"));
   Serial.println(F("[T] play audio Tone"));
   Serial.println(F("[P] PWM/Buzzer out"));
 
@@ -95,13 +93,13 @@ void loop() {
     }
   }
 
-  char command = Serial.read();
+  char command = Serial.read(); // sets command as a vairable where it will print the value into the serial monitor so it is run
   Serial.println(command);
 
 
   switch (command) {
     case '?': {
-        printMenu();
+        printMenu();// The printMenu function prints out the initial menu so the user can see it once again, can be changed out later down the line
         break;
       }
 
@@ -109,11 +107,11 @@ void loop() {
 
     case 'n': {
         // read the network/cellular status
-        uint8_t n = fona.getNetworkStatus();
+        uint8_t n = fona.getNetworkStatus(); // This function returns a number based on the status of the mobile network which the arduino prints into the serial monitor
         Serial.print(F("Network status "));
         Serial.print(n);
         Serial.print(F(": "));
-        if (n == 0) Serial.println(F("Not registered"));
+        if (n == 0) Serial.println(F("Not registered")); // number 1-5 is returned based on the result
         if (n == 1) Serial.println(F("Registered (home)"));
         if (n == 2) Serial.println(F("Not registered (searching)"));
         if (n == 3) Serial.println(F("Denied"));
@@ -126,16 +124,16 @@ void loop() {
     case 'v': {
         // set volume
         flushSerial();
-        if ( (type == FONA3G_A) || (type == FONA3G_E) ) {
+        if ( (type == FONA3G_A) || (type == FONA3G_E) ) { // checks the type of FONA module as some modules display their volume percentage in a different format
           Serial.print(F("Set Vol [0-8] "));
         } else {
-          Serial.print(F("Set Vol % [0-100] "));
+          Serial.print(F("Set Vol % [0-100] ")); // This option will be used for my circuit as I am using something other than the two modules above, they are kept in the if statement in case they are used to replicate my project
         }
-        uint8_t vol = readnumber();
+        uint8_t vol = readnumber(); // a unint8_t is an unsigned integer which allows us to save memory on the arduino
         Serial.println();
         if (! fona.setVolume(vol)) {
           Serial.println(F("Failed"));
-        } else {
+        } else {   // Prints the final result of the operation
           Serial.println(F("OK!"));
         }
         break;
@@ -144,7 +142,7 @@ void loop() {
     case 'V': {
         uint8_t v = fona.getVolume();
         Serial.print(v);
-        if ( (type == FONA3G_A) || (type == FONA3G_E) ) {
+        if ( (type == FONA3G_A) || (type == FONA3G_E) ) { // Different models use different methods of storing volume level
           Serial.println(" / 8");
         } else {
           Serial.println("%");
@@ -152,33 +150,11 @@ void loop() {
         break;
       }
 
-    case 'H': {
-        // Set Headphone output
-        if (! fona.setAudio(FONA_HEADSETAUDIO)) {
-          Serial.println(F("Failed"));
-        } else {
-          Serial.println(F("OK!"));
-        }
-        fona.setMicVolume(FONA_HEADSETAUDIO, 15);
-        break;
-      }
-    case 'e': {
-        // Set External output
-        if (! fona.setAudio(FONA_EXTAUDIO)) {
-          Serial.println(F("Failed"));
-        } else {
-          Serial.println(F("OK!"));
-        }
-
-        fona.setMicVolume(FONA_EXTAUDIO, 10);
-        break;
-      }
-
     case 'T': {
         // play tone
         flushSerial();
         Serial.print(F("Play tone #"));
-        uint8_t kittone = readnumber();
+        uint8_t kittone = readnumber(); // Plays a sound which is useful for checking audio connection
         Serial.println();
         // play for 1 second (1000 ms)
         if (! fona.playToolkitTone(kittone, 1000)) {
@@ -190,17 +166,17 @@ void loop() {
       }
 
     /*** PWM ***/
-
+// My model of FONA module has an empty PWM pin, PWM stands for pulsewidth modulation which allows the headers to control variable current components such as motors, not in use in the project but left in in case it became useful in the future
     case 'P': {
         // PWM Buzzer output @ 2KHz max
         flushSerial();
-        Serial.print(F("PWM Freq, 0 = Off, (1-2000): "));
+        Serial.print(F("PWM Freq, 0 = Off, (1-2000): ")); // Selects the desired frequency to run the PWM headers at from 1 to 2000 KHz
         uint16_t freq = readnumber();
         Serial.println();
         if (! fona.setPWM(freq)) {
           Serial.println(F("Failed"));
         } else {
-          Serial.println(F("OK!"));
+          Serial.println(F("OK!")); // Prints final result
         }
         break;
       }
@@ -208,7 +184,7 @@ void loop() {
     /*** Call ***/
     case 'c': {
         // call a phone!
-        char number[30];
+        char number[30]; // Creates the array number with the size of 30 so it can be acsessed from the rest of the function 
         flushSerial();
         Serial.print(F("Call #"));
         readline(number, 30);
@@ -217,14 +193,14 @@ void loop() {
         if (!fona.callPhone(number)) {
           Serial.println(F("Failed"));
         } else {
-          Serial.println(F("Sent!"));
+          Serial.println(F("Sent!")); // Shows result of function
         }
 
         break;
       }
     case 'A': {
         // get call status
-        int8_t callstat = fona.getCallStatus();
+        int8_t callstat = fona.getCallStatus(); // function to check the status of the current call whether it is ready, ringing, ongoing or has some sort of error
         switch (callstat) {
           case 0: Serial.println(F("Ready")); break;
           case 1: Serial.println(F("Could not get status")); break;
@@ -237,7 +213,7 @@ void loop() {
       
     case 'h': {
         // hang up!
-        if (! fona.hangUp()) {
+        if (! fona.hangUp()) { // this function ends the current call
           Serial.println(F("Failed"));
         } else {
           Serial.println(F("OK!"));
@@ -247,7 +223,7 @@ void loop() {
 
     case 'p': {
         // pick up!
-        if (! fona.pickUp()) {
+        if (! fona.pickUp()) { // this function answers the current caller
           Serial.println(F("Failed"));
         } else {
           Serial.println(F("OK!"));
@@ -259,7 +235,7 @@ void loop() {
 
     case 'N': {
         // read the number of SMS's!
-        int8_t smsnum = fona.getNumSMS();
+        int8_t smsnum = fona.getNumSMS(); // checks the number of SMS messages that are stored 
         if (smsnum < 0) {
           Serial.println(F("Could not read # SMS"));
         } else {
@@ -270,7 +246,7 @@ void loop() {
       }
     case 'r': {
         // read an SMS
-        flushSerial();
+        flushSerial(); // gives a list of numbers that messages have been recieved from and lets the user choose which one is to be read and printed
         Serial.print(F("Read #"));
         uint8_t smsn = readnumber();
         Serial.print(F("\n\rReading SMS #")); Serial.println(smsn);
@@ -297,11 +273,11 @@ void loop() {
       }
     case 'R': {
         // read all SMS
-        int8_t smsnum = fona.getNumSMS();
-        uint16_t smslen;
-        int8_t smsn;
+        int8_t smsnum = fona.getNumSMS(); // prints all SMS at once
+        uint16_t smslen; // creates unsinged value which represents the length of a message
+        int8_t smsn; // signed integer to store the number of messages
 
-        if ( (type == FONA3G_A) || (type == FONA3G_E) ) {
+        if ( (type == FONA3G_A) || (type == FONA3G_E) ) { // options used for other FONA models which have different ways of indexing data
           smsn = 0; // zero indexed
           smsnum--;
         } else {
@@ -315,7 +291,7 @@ void loop() {
             break;
           }
           // if the length is zero, its a special case where the index number is higher
-          // so increase the max we'll look at!
+          // this if statement fixes an error that arises if a message with no data is recieved
           if (smslen == 0) {
             Serial.println(F("[empty slot]"));
             smsnum++;
@@ -338,16 +314,16 @@ void loop() {
 
         Serial.print(F("\n\rDeleting SMS #")); Serial.println(smsn);
         if (fona.deleteSMS(smsn)) {
-          Serial.println(F("OK!"));
+          Serial.println(F("OK!")); // uses functions and values of smsn to select and delete the correct method
         } else {
-          Serial.println(F("Couldn't delete"));
+          Serial.println(F("Couldn't delete")); // returns an error message if the message cannot be deleted
         }
         break;
       }
 
     case 's': {
         // send an SMS!
-        char sendto[21], message[141];
+        char sendto[21], message[141]; // creates variables for the number to be sent to and the message with a character limit of 140, both as arrays
         flushSerial();
         Serial.print(F("Send to #"));
         readline(sendto, 20);
@@ -365,7 +341,7 @@ void loop() {
       }
 
     case 'u': {
-      // send a USSD! ONLY USED IN IOT
+      // send a USSD! ONLY USED IN IOT, this sends a very short message which can be used for triggers for smart devices etc etc
       char message[141];
       flushSerial();
       Serial.print(F("Type out one-line message (140 char): "));
@@ -384,29 +360,14 @@ void loop() {
       }
     }
 
-
-    case 'S': {
-        Serial.println(F("Creating SERIAL TUBE"));
-        while (1) {
-          while (Serial.available()) {
-            delay(1);
-            fona.write(Serial.read());
-          }
-          if (fona.available()) {
-            Serial.write(fona.read());
-          }
-        }
-        break;
-      }
-
     default: {
-        Serial.println(F("Unknown command"));
+        Serial.println(F("Unknown command")); // if an input that is not in the menu is recieved the program will return an error and reprint the starting screen
         printMenu();
         break;
       }
   }
   // flush input
-  flushSerial();
+  flushSerial(); // clears current data
   while (fona.available()) {
     Serial.write(fona.read());
   }
